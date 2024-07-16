79107af6ad26eda7d4efe74df9c886c435907d63
icegosimperson <hooho931@gmail.com>
2024-07-16 22:57:11 +0900

    settings

---
+++ src/main/java/com/ll/JollyJourney/JollyJourneyApplication.java
@@ -0,0 +1,13 @@
+package com.ll.JollyJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JollyJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JollyJourneyApplication.class, args);
+	}
+
+}
