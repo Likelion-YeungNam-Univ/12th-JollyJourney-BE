8d972deb6ec7f57da49571fdaa89987821b33dcb
gayoi <117894300+gayoi@users.noreply.github.com>
2024-07-23 01:19:46 +0900

    feat: loginType 추가

---
+++ src/main/java/com/ll/JollyJourney/global/enums/LoginType.java
@@ -0,0 +1,14 @@
+package com.ll.JollyJourney.global.enums;
+
+public enum LoginType {
+
+    KAKAO("KAKAO"),
+    APP("APP");
+
+    private String value;
+
+    LoginType(String loginType) {
+
+    }
+
+}
