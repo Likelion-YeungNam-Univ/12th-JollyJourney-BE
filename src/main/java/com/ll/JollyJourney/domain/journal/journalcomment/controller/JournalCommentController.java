579ce87f38c2c6b4c86e198aaf2f0e4ec6d675d3
icegosimperson <hooho931@gmail.com>
2024-07-31 18:29:08 +0900

    feat(api): comments api 변경
    journal/{journalId}/comments

--- src/main/java/com/ll/JollyJourney/domain/journal/journalcomment/controller/JournalCommentController.java
+++ src/main/java/com/ll/JollyJourney/domain/journal/journalcomment/controller/JournalCommentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/journal/{journalId}")
+@RequestMapping("/journal/{journalId}/comments")
 public class JournalCommentController {
     @Autowired
     private JournalCommentService journalCommentService;
