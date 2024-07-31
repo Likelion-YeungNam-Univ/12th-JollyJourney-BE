75f78986137c06054147f609991a38a4fa33b783
icegosimperson <hooho931@gmail.com>
2024-07-31 19:36:56 +0900

    feat: LikesReq 구현
    memberId, journalId

---
+++ src/main/java/com/ll/JollyJourney/domain/likes/dto/LikesReq.java
@@ -0,0 +1,19 @@
+package com.ll.JollyJourney.domain.likes.dto;
+
+import com.ll.JollyJourney.domain.journal.journal.entity.Journal;
+import com.ll.JollyJourney.domain.likes.entity.Likes;
+import com.ll.JollyJourney.domain.member.member.entity.Member;
+import jakarta.validation.constraints.NotNull;
+
+public record LikesReq(
+        @NotNull Long memberId,
+        @NotNull Long journalId
+) {
+    public Likes toEntity(Journal journal, Member member) {
+        return Likes.builder()
+                .member(member)
+                .journal(journal)
+                .build();
+    }
+}
+
