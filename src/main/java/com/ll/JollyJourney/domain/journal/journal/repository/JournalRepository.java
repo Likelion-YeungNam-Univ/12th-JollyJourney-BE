cc854b39f2ef4c7c94ccce7951317f7666306a49
icegosimperson <hooho931@gmail.com>
2024-07-31 11:01:52 +0900

    fix: REST API로 변경
    Optional

--- src/main/java/com/ll/JollyJourney/domain/journal/journal/repository/JournalRepository.java
+++ src/main/java/com/ll/JollyJourney/domain/journal/journal/repository/JournalRepository.java
@@ -6,14 +6,30 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public interface JournalRepository extends JpaRepository<Journal, Long> {
-    Journal findByTitle(String title);
-    Journal findByTitleAndContent(String title, String content);
+    Optional<Journal> findByTitle(String title);
+    Optional<Journal> findByTitleAndContent(String title, String content);
     List<Journal> findByTitleLike(String title);
     Page<Journal> findAll(Pageable pageable);
+
+    /*
+    @PersistenceContext;
+    EntityManager em;
+
+    public void save(Journal journal) {
+        em.persist(journal);
+    }
+    public Journal findjournal(Long id) {}
+    public List<Journal> findAll() {}
+
+
+    */
 
     @Query("SELECT j FROM Journal j WHERE (:type = 'title' AND j.title LIKE %:keyword%) OR (:type = 'content' AND j.content LIKE %:keyword%)")
     Page<Journal> searchByTypeAndKeyword(@Param("type") String type, @Param("keyword") String keyword, Pageable pageable);
