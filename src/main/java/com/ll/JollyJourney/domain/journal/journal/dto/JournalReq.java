e8b4bf8f8ce1e5d06058d7cd110f42280c62f31c
icegosimperson <hooho931@gmail.com>
2024-07-31 17:20:52 +0900

    fix: memberId 삭제
    관리자만 글을 작성할 수 있으니 삭제

--- src/main/java/com/ll/JollyJourney/domain/journal/journal/dto/JournalReq.java
+++ src/main/java/com/ll/JollyJourney/domain/journal/journal/dto/JournalReq.java
@@ -1,17 +1,15 @@
 package com.ll.JollyJourney.domain.journal.journal.dto;
 
 import com.ll.JollyJourney.domain.journal.journal.entity.Journal;
-import com.ll.JollyJourney.domain.member.member.entity.Member;
 import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 
 public record JournalReq(
         @NotNull String title,
         String content,
-        Long memberId
+        Long userId
 ) {
-    public Journal toEntity(Member member){
+    public Journal toEntity(){
         return Journal.builder()
-                .member(member)
                 .title(this.title)
                 .content(this.content)
                 .build();
