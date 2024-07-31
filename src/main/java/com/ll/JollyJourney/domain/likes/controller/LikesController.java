2f4db04f21b2340a0a7757db97822820823c8fd3
icegosimperson <hooho931@gmail.com>
2024-07-31 19:43:10 +0900

    feat: 좋아요 추가, 취소 기능 REST API로 구현

--- src/main/java/com/ll/JollyJourney/domain/likes/controller/LikesController.java
+++ src/main/java/com/ll/JollyJourney/domain/likes/controller/LikesController.java
@@ -1,7 +1,12 @@
 package com.ll.JollyJourney.domain.likes.controller;
 
+import com.ll.JollyJourney.domain.likes.dto.LikesReq;
+import com.ll.JollyJourney.domain.likes.dto.LikesRes;
+import com.ll.JollyJourney.domain.likes.entity.Likes;
 import com.ll.JollyJourney.domain.likes.service.LikesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,18 +14,27 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/likes")
 public class LikesController {
-
+    @Autowired
     private final LikesService likesService;
 
     @PostMapping
-    public String addLike(@RequestParam Long journalId, @RequestParam Long memberId) {
-        likesService.likeJournal(journalId, memberId);
-        return "redirect:/journal/detail/" + journalId;
+    public ResponseEntity<LikesRes> addLike(@RequestBody LikesReq likesReq) {
+        Likes likes = likesService.addLikes(likesReq);
+        LikesRes likesRes = LikesRes.fromEntity(likes);
+        return ResponseEntity.ok(likesRes);
     }
 
+    @DeleteMapping("/{likesId}")
+    public ResponseEntity<Void> removeLike(@PathVariable Long likesId) {
+        likesService.deleteLikes(likesId);
+        return ResponseEntity.ok().build();
+    }
+}
+
+/*
     @DeleteMapping
     public String removeLike(@RequestParam Long journalId, @RequestParam Long memberId) {
         likesService.likeJournal(journalId, memberId);
         return "redirect:/journal/detail/" + journalId;
     }
-}
+ */
