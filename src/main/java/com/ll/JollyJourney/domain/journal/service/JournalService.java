7569508bb05d485231ff62b123f36b272255e30d
icegosimperson <hooho931@gmail.com>
2024-08-02 13:15:35 +0900

    feat: 정보글 삭제시 예외처리 추가

--- src/main/java/com/ll/JollyJourney/domain/journal/service/JournalService.java
+++ src/main/java/com/ll/JollyJourney/domain/journal/service/JournalService.java
@@ -48,68 +48,8 @@ public class JournalService {
 
     @Transactional
     public void deleteJournal(Long journalId) {
-        journalRepository.deleteById(journalId);
-    }
-}
-
-
-
-
-/*
-@RequiredArgsConstructor
-@Service
-public class JournalService {
-    private final JournalRepository journalRepository;
-
-    public List<Journal> getList() {
-        return this.journalRepository.findAll();
-    }
-
-    public Journal getJournal(Long id) {
-        Optional<Journal> journal = this.journalRepository.findById(id);
-        if (journal.isPresent()) {
-            return journal.get();
-        } else {
-            throw new DataNotFoundException("journal not found");
-        }
-    }
-    public void create(String title, String content) {
-        Journal journal = Journal.builder()
-                .title(title)
-                .content(content)
-                .createDate(LocalDateTime.now())
-                .modifyDate(LocalDateTime.now())
-                .likesCount(0)
-                .build();
-        journalRepository.save(journal);
-    }
-
-    @Transactional
-    public void modify(Long journalId, String title, String content) {
         Journal journal = journalRepository.findById(journalId)
-                .orElseThrow(() -> new DataNotFoundException("Journal not found"));
-
-        journal.updateJournal(title, content);
+                .orElseThrow(() -> new IllegalArgumentException("해당 정보글 없음"));
+        journalRepository.delete(journal);
     }
-
-    public void delete(Journal journal) {
-        this.journalRepository.delete(journal);
-    }
-    public Page<Journal> getList(int page) {
-        List<Sort.Order> sorts = new ArrayList<>();
-        sorts.add(Sort.Order.desc("createDate"));
-        Pageable pageable = PageRequest.of(page, 3, Sort.by(sorts));
-        return this.journalRepository.findAll(pageable);
-    }
-
-    public Page<Journal> search(String type, String keyword, int page) {
-        String searchKeyword = "%" + keyword + "%";
-        List<Sort.Order> sorts = new ArrayList<>();
-        sorts.add(Sort.Order.desc("createDate"));
-        Pageable pageable = PageRequest.of(page, 3, Sort.by(sorts));
-        return this.journalRepository.searchByTypeAndKeyword(type, keyword, pageable);
-    }
-
 }
-
- */
