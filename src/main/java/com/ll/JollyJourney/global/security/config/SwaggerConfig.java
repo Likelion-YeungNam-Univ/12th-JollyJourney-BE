61103ac555a0ed59c9d6cecfcb87be601d17162b
icegosimperson <hooho931@gmail.com>
2024-08-05 03:54:24 +0900

    feat: Swagger SecuritySchemes 추가

--- src/main/java/com/ll/JollyJourney/global/security/config/SwaggerConfig.java
+++ src/main/java/com/ll/JollyJourney/global/security/config/SwaggerConfig.java
@@ -1,24 +1,37 @@
 package com.ll.JollyJourney.global.security.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
-        info = @Info(title = "Test App",
-                description = "TestApp API 명세",
+        info = @Info(title = "Jolly Journey",
+                description = "Jolly Journey API 명세",
                 version = "v1")
 )
+@SecuritySchemes({
+        @SecurityScheme(
+                name = "BearerAuth",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT",
+                in = SecuritySchemeIn.HEADER
+        )
+})
 public class SwaggerConfig {
     @Bean
     public GroupedOpenApi openApi() {
         String[] paths = {"/**"};
 
         return GroupedOpenApi.builder()
-                .group("TestApp API v1")
+                .group("Jolly Journey website API v1")
                 .pathsToMatch(paths)
                 .build();
     }
