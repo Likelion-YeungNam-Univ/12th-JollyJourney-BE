c93662b1694aa48ce092efe3bafe9b47f77ec091
icegosimperson <hooho931@gmail.com>
2024-08-05 16:00:52 +0900

    fix: journalComment Swagger 추가

--- src/main/java/com/ll/JollyJourney/domain/journalcomment/entity/JournalComment.java
+++ src/main/java/com/ll/JollyJourney/domain/journalcomment/entity/JournalComment.java
@@ -1,5 +1,6 @@
 package com.ll.JollyJourney.domain.journalcomment.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.ll.JollyJourney.domain.journal.entity.Journal;
 import com.ll.JollyJourney.domain.member.member.entity.Member;
 import com.ll.JollyJourney.global.jpa.BaseEntity;
@@ -19,10 +20,12 @@ public class JournalComment extends BaseEntity {
 
     @JoinColumn(name = "journal_id")
     @ManyToOne(fetch = FetchType.EAGER)
+    @JsonBackReference
     private Journal journal;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "member_id")
+    @JsonBackReference
     private Member member;
 
     @Column(length = 100)
@@ -30,5 +33,8 @@ public class JournalComment extends BaseEntity {
 
     public void setContent(String content) {
         this.content = content;
+    }
+    public Long getJournalId() { // 이 메서드 추가
+        return this.journal.getJournalId();
     }
 }
--- src/main/java/com/ll/JollyJourney/domain/journalcomment/repository/JournalCommentRepository.java
+++ src/main/java/com/ll/JollyJourney/domain/journalcomment/repository/JournalCommentRepository.java
@@ -1,11 +1,12 @@
 package com.ll.JollyJourney.domain.journalcomment.repository;
 
-import com.ll.JollyJourney.domain.journal.entity.Journal;
 import com.ll.JollyJourney.domain.journalcomment.entity.JournalComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
+@Repository
 public interface JournalCommentRepository extends JpaRepository<JournalComment, Long> {
-    Optional<JournalComment> findByJournal(Journal journal);
+    List<JournalComment> findByJournal_JournalId(Long journalId); // 특정 journalId의 모든 댓글을 반환하는 메서드
 }
