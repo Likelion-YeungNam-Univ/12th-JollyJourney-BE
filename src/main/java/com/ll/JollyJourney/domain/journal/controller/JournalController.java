0ec45c185b522634087f0a77e93d873b25957186
icegosimperson <hooho931@gmail.com>
2024-08-05 16:53:37 +0900

    fix: JournalReq userId 삭제
    삭제 이유 : Authentication으로 인증 가능

--- src/main/java/com/ll/JollyJourney/domain/journal/controller/JournalController.java
+++ src/main/java/com/ll/JollyJourney/domain/journal/controller/JournalController.java
@@ -9,7 +9,6 @@ import com.ll.JollyJourney.global.service.S3Service;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -39,28 +38,7 @@ public class JournalController {
     @Operation(summary = "정보글 전체 조회", description = "정보글 전체 조회")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "정보글 전체 조회 성공",
-                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
-                    [
-                        {
-                            "journalId": 1,
-                            "title": "테스트 데이터입니다:[001]",
-                            "content": "내용무",
-                            "createDate": "2024-07-23T21:51:49.252343",
-                            "modifyDate": "2024-08-03T12:15:17.442176",
-                            "likesCount": 0,
-                            "commentCount": 2
-                        },
-                        {
-                            "journalId": 2,
-                            "title": "테스트 데이터입니다:[002]",
-                            "content": "내용무",
-                            "createDate": "2024-07-23T21:51:49.415665",
-                            "modifyDate": "2024-07-23T21:51:49.42922",
-                            "likesCount": 0,
-                            "commentCount": 0
-                        }
-                    ]
-                    """))),
+                    content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "400", description = "전체 정보글 조회 실패")
     })
     @GetMapping("")
@@ -72,17 +50,7 @@ public class JournalController {
     @Operation(summary = "특정 정보글 조회", description = "journalId로 특정 게시물을 조회하는 기능")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "정보글 조회 성공",
-                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
-                            {
-                                "journalId": 1,
-                                "title": "테스트 데이터입니다:[001]",
-                                "content": "내용무",
-                                "createDate": "2024-07-23T21:51:49.252343",
-                                "modifyDate": "2024-08-03T12:15:17.442176",
-                                "likesCount": 0,
-                                "commentCount": 2
-                            }
-                            """))),
+                    content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "400", description = "정보글 조회 실패")
     })
     @GetMapping("/{id}")
@@ -96,17 +64,7 @@ public class JournalController {
             security = @SecurityRequirement(name = "BearerAuth"))
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "정보글 생성 성공",
-                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
-                            {
-                                "journalId": 1,
-                                "title": "새로운 정보글 제목",
-                                "content": "새로운 정보글 내용",
-                                "createDate": "2024-07-23T21:51:49.252343",
-                                "modifyDate": "2024-08-03T12:15:17.442176",
-                                "likesCount": 0,
-                                "commentCount": 0
-                            }
-                            """))),
+                    content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "400", description = "정보글 생성 실패")
     })
     @PreAuthorize("hasRole('ADMIN')")
@@ -114,7 +72,6 @@ public class JournalController {
     public ResponseEntity<Journal> createJournal(
             @Parameter(description = "정보글 제목", required = true) @RequestParam("title") String title,
             @Parameter(description = "정보글 내용", required = true) @RequestParam("content") String content,
-            @Parameter(description = "사용자 ID", required = true) @RequestParam("userId") Long userId,
             @Parameter(description = "이미지 파일", required = true,
                     content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
                             schema = @Schema(type = "string", format = "binary"))) @RequestPart("image") MultipartFile image) {
@@ -123,7 +80,7 @@ public class JournalController {
             String imageUrl = s3Service.uploadFile(image);
 
             // 게시글 생성
-            JournalReq request = new JournalReq(title, content, userId, image);
+            JournalReq request = new JournalReq(title, content, image);
             Journal journal = request.toEntity(imageUrl);
             Journal savedJournal = journalService.createJournal(request, imageUrl);
 
@@ -137,17 +94,7 @@ public class JournalController {
             security = @SecurityRequirement(name = "BearerAuth"))
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "정보글 수정 성공",
-                    content = @Content(mediaType = "application/json", examples = @ExampleObject(value = """
-                            {
-                                "journalId": 1,
-                                "title": "수정된 정보글 제목",
-                                "content": "수정된 정보글 내용",
-                                "createDate": "2024-07-23T21:51:49.252343",
-                                "modifyDate": "2024-08-03T12:15:17.442176",
-                                "likesCount": 0,
-                                "commentCount": 0
-                            }
-                            """))),
+                    content = @Content(mediaType = "application/json")),
             @ApiResponse(responseCode = "400", description = "정보글 수정 실패")
     })
     @PreAuthorize("hasRole('ADMIN')")
@@ -156,7 +103,6 @@ public class JournalController {
             @PathVariable Long id,
             @Parameter(description = "정보글 제목", required = true) @RequestParam("title") String title,
             @Parameter(description = "정보글 내용", required = true) @RequestParam("content") String content,
-            @Parameter(description = "사용자 ID", required = true) @RequestParam("userId") Long userId,
             @Parameter(description = "이미지 파일", required = false, content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE, schema = @Schema(type = "string", format = "binary"))) @RequestPart(value = "image", required = false) MultipartFile image) {
         try {
             // 이미지 업로드
@@ -165,7 +111,7 @@ public class JournalController {
                 imageUrl = s3Service.uploadFile(image);
             }
             // 게시글 수정
-            JournalReq request = new JournalReq(title, content, userId, image);
+            JournalReq request = new JournalReq(title, content, image);
             Journal journal = journalService.updateJournal(id, request, imageUrl);
             JournalRes journalRes = JournalRes.fromEntity(journal);
             return ResponseEntity.ok(journalRes);
