d8c9749f365828544d4c8adf4045dfc4febbdfb8
gayoi <117894300+gayoi@users.noreply.github.com>
2024-08-05 18:01:18 +0900

    fix: 오류수정

--- src/main/java/com/ll/JollyJourney/domain/auth/controller/AuthController.java
+++ src/main/java/com/ll/JollyJourney/domain/auth/controller/AuthController.java
@@ -19,26 +19,26 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
-    @CrossOrigin(origins = "http://localhost:8080")
+
     @PostMapping("/signin")
     public ResponseEntity<?> signIn(@RequestBody SignInReq request) {
         return ResponseEntity.ok(authService.signIn(request));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody SignUpReq request) {
         return ResponseEntity.ok(authService.signUp(request));
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+
     @PutMapping("/modify")
     public ResponseEntity<Member> modifyInfo(@RequestBody ModifyInfoReq request) {
         Member updatedMember = authService.modifyInfo(request);
         return ResponseEntity.ok(updatedMember);
     }
 
-    @CrossOrigin(origins = "http://localhost:8080")
+
     @PutMapping("/password/update")
     public ResponseEntity<Member> changePassword(@RequestBody ChangePasswordReq request) {
         Member updatedMember = authService.changePassword(request);
