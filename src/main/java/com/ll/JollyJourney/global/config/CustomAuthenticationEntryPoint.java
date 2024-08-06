009ca9b1b919ad6120781ef38649cc3a9c4e5395
gayoi <117894300+gayoi@users.noreply.github.com>
2024-08-06 22:34:05 +0900

    fix: cors 에러

--- src/main/java/com/ll/JollyJourney/global/config/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/ll/JollyJourney/global/config/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,30 @@
 package com.ll.JollyJourney.global.config;
 
+//@Component
+//public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+//
+//    private final ObjectMapper objectMapper = new ObjectMapper();
+//
+//    @Override
+//    public void commence(HttpServletRequest request,
+//                         HttpServletResponse response,
+//                         AuthenticationException authException) throws IOException, ServletException {
+//
+//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        response.setCharacterEncoding("utf-8");
+//        response.setContentType("application/json");
+//        final ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
+//                403,
+//                "인증되지 않은 사용자"
+//        );
+//
+//        ApiResponse<?> apiResponse = ApiResponse.createError(apiExceptionResponse);
+//        String jsonResponse = objectMapper.writeValueAsString(apiResponse);
+//
+//        response.getWriter().write(jsonResponse);
+//    }
+//}
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
